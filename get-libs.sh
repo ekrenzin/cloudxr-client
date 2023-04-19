1d2fdcbed08e4ea217d7de699fcca8eea4291c23
Rexios <rexios@rexios.dev>
2023-04-19 15:49:58 -0400

    Add script to download libs

---
+++ get-libs.sh
@@ -0,0 +1,4 @@
+mkdir app/libs
+curl https://github.com/ekrenzin/cloudxr-client-libraries/raw/main/CloudXR.aar > app/libs/CloudXR.aar
+curl https://github.com/ekrenzin/cloudxr-client-libraries/raw/main/oboe-1.5.0.aar > app/libs/oboe.aar
+curl https://github.com/ekrenzin/cloudxr-client-libraries/raw/main/ovr_sdk.zip > app/libs/ovr_sdk.zip
